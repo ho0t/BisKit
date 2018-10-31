7b98392b1bed42723343413dc785b9c71a3e340c
Nicola <nicorsm@me.com>
2018-10-31 21:37:37 +0100

    Project setup

---
+++ Poppo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poppo
+//
+//  Created by Nicola on 31/10/18.
+//  Copyright © 2018 ho0t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
